c9bf58d8a1885d5075d4ce0e0761112b05188fe6
Miklos Aszriev <fotoamg@gmail.com>
2019-03-23 23:42:04 +0100

    Adding the /photos/food response first version.

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ const Flickr = require("flickrapi"),
 fastify.get('/', async (request, reply) => {
     reply.header('Content-Type', 'text/html')
     reply.type('text/html')
-    return `<H1>Please use the api at /api/photos...</H1>
-            <p>Or calling photo list at<a href="/photos/food">/photos/food</a></p>
+    return `<H1>Please use the api at <a href="/api/photos">/api/photos</a> </H1>
+            <p>Or calling photo list at <a href="/photos/food">/photos/food</a></p>
             <HR/>`;
  })
 
@@ -50,6 +50,18 @@ fastify.get('/', async (request, reply) => {
     reply.header('Content-Type', 'text/html')
     reply.type('text/html')
     return `Hello World`;
+ })
+
+ fastify.get('/photos/food', async (request, reply) => {
+    reply.header('Content-Type', 'text/html')
+    reply.type('text/html')
+    const photos = await Photo.find();
+    let payload = `<ul>`;
+    photos.forEach((photo, index) => {
+        payload+=`<li><a href="${photo.url}"> ID: ${photo.id} </a> &nbsp; Taken:${photo.taken} `;
+    })
+    payload +=`</ul>`;
+    return payload;
  })
   
 Flickr.tokenOnly(flickrOptions, function(error, flickr) {
@@ -79,8 +91,8 @@ Flickr.tokenOnly(flickrOptions, function(error, flickr) {
                         lastupdate : result.photo.dates.lastupdate,
                         url : result.photo.urls.url[0]._content
                    });
-                   console.log('X');
-                   // photo.save()
+                   //console.log('X');
+                   // photo.save() // commented out as no delete at the moment and scheduler..... run it once to populate DB!
                   } catch (err) {
                     throw boom.boomify(err)
                   }
@@ -93,7 +105,6 @@ Flickr.tokenOnly(flickrOptions, function(error, flickr) {
 
 });
 console.log("App started");
-
 
 // Run the server!
 const start = async () => {
